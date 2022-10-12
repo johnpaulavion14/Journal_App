566ac8c9e8b192ee6382a8c0334293891aa4888e
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-12 13:25:42 +0800

    Add code in dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @tasks_today = current_user.tasks.all.where(date:Date.today).order(time: :asc)
+    @article_names = @tasks_today.map {|task| task.category.category}
   end
 end
