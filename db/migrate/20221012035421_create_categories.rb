42b46f13b0070c11211f291b32797aa50b21e215
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-10-19 16:06:47 +0800

    Update schema

--- db/migrate/20221012035421_create_categories.rb
+++ db/migrate/20221012035421_create_categories.rb
@@ -3,8 +3,10 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :category
       t.string :description
+      t.integer :user_id
 
       t.timestamps
     end
+    add_index :categories, :user_id
   end
 end
--- db/migrate/20221012035644_create_tasks.rb
+++ db/migrate/20221012035644_create_tasks.rb
@@ -4,8 +4,10 @@ class CreateTasks < ActiveRecord::Migration[7.0]
       t.string :task
       t.time :time
       t.date :date
+      t.integer :category_id
 
       t.timestamps
     end
+    add_index :tasks, :category_id
   end
 end
