c2688000df991e277236dfaaa925f97818645beb
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2022-12-31 13:07:47 +0800

    test push

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_043219) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_035644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
     t.string "description"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
@@ -27,9 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_043219) do
     t.string "task"
     t.time "time"
     t.date "date"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
